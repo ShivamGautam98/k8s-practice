22c2c649d4ab26d32b1b3a19e90a3fd37529f486
Sneha Gautam <gautam.shivam98@gmail.com>
2023-08-17 19:52:49 +0530

    adding config files for k8s components

---
+++ demo-app/deploy.sh
@@ -0,0 +1,14 @@
+kubectl get pod # get all pods
+kubectl apply -f mongo-config.yaml
+kubectl apply -f mongo-secret.yaml
+kubectl apply -f mongo.yaml
+kubectl apply -f webapp.yaml
+kubectl get all # get all components
+kubectl get configmap
+kubectl get secret
+kubectl get svc # for service
+# minikube ip # known issue with Minikube 
+minikube service webapp-service # https://minikube.sigs.k8s.io/docs/handbook/accessing/#example-of-nodeport
+# kubectl get <component name> : Brief Description
+# kubectl describe <component name> : Detailed Description
+# kubectl logs <pod-id> : For troubleshooting
